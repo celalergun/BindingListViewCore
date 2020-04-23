adbc55462fcf0b2b829be2baf4d199a2c45a9642
Celal Ergun <celalergun@gmail.com>
2020-04-23 12:35:17 +0300

    Forked from https://github.com/geomatics-io/BindingListView.

---
+++ BindingListViewCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyDescription("Advanced, data bindable, object list view.")]
+[assembly: AssemblyCopyright("Copyright © Andrew Davey 2005")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM componenets.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("709a6d28-582d-423b-ae59-83e0945c4227")]
+
+[assembly: CLSCompliant(true)]
